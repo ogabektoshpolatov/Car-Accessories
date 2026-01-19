5fdefd24f766bbd41cd54faa852192e2af49abc8
ogabek_toshpolatov <ogabektoshpolatov2002@gmail.com>
2026-01-19 10:48:46 +0500

    Added change for ShopLayoutPage

--- src/backend/CarAccessories.Client/Program.cs
+++ src/backend/CarAccessories.Client/Program.cs
@@ -3,3 +3,4 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 
 await builder.Build().RunAsync();
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
---
+++ src/backend/CarAccessories/Components/Pages/Home.razor.cs
@@ -0,0 +1,15 @@
+﻿
+using System.Net;
+using CarAccessories.Shared.Responses;
+
+namespace CarAccessories.Components.Pages;
+
+public partial class Home
+{
+    private CategoryResponseModel[] categories;
+
+    protected override async Task OnInitializedAsync()
+    {
+        categories = await WebRequestMethods.Http.GetFromJsonAsync<CategoryResponseModel[]>("WeatherForecast");
+    }
+}
